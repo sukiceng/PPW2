183422899105b76e7d1565d9650c83df4f24afc1
syauqitaqiyuddin <ahmadsyauqi150@gmail.com>
2023-11-07 20:55:39 +0700

    initial commit

---
+++ app/Http/Controllers/Auth/LoginRegisterController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+
+
+
+class LoginRegisterController extends Controller
+{
+    /**
+     * Instantiate a new LoginRegisterController instance.
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except([
+            'logout', 'dashboard'
+        ]);
+    }
+
+    /**
+     * Display a registration form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Store a new user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:250',
+            'email' => 'required|email|max:250|unique:users',
+            'password' => 'required|min:8|confirmed',
+            'photo' => 'image|nullable|max:1999'
+        ]);
+
+        //periksa apakah file sudah diupload
+        if ($request->hasFile('photo')) {
+            $image = $request->file('photo');
+
+            // buat unique file name
+            $filename = 'profile_image_' . time() . '.' . $image->getClientOriginalExtension();
+            // simpan gambar original
+            $image->storeAs('public/profile_images',$filename);
+            // create thumbnail
+            $thumbnail = Image::make($image)->fit(100, 100)->stream();
+            $squareThumbnail = Image::make($image)->fit(200, 200)->stream();
+            // store thumbnail
+            Storage::put('public/profile_images/thumbnail_' . $filename, $thumbnail);
+            Storage::put('public/profile_images/square_' . $filename, $squareThumbnail);
+        }else{
+            $filename = 'noimage.jpg';
+        }
+
+        $path = 'noimage.jpg';
+
+        if($request->hasFile('picture')){
+            $filenameWithExt = $request->file('photo')->getClientOriginalName();
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            $extension = $request->file('photo')->getClientOriginalExtension();
+            $filenameSimpan = $filename . '_' . time() . '.' . $extension;
+            $path = $request->file('photo')->storeAs('photo', $filenameSimpan);
+        } else {
+            $filenameSimpan = 'noimage.jpg';
+        }
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'photo' => $path
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        Auth::attempt($credentials);
+        $request->session()->regenerate();
+        return redirect()->route('dashboard')
+        ->withSuccess('You have successfully registered & logged in!');
+
+    }
+
+    /**
+     * Display a login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Authenticate the user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($credentials))
+        {
+            $request->session()->regenerate();
+            return redirect()->route('dashboard')
+                ->withSuccess('You have successfully logged in!');
+        }
+
+        return back()->withErrors([
+            'email' => 'Your provided credentials do not match in our records.',
+        ])->onlyInput('email');
+
+    } 
+    
+    /**
+     * Display a dashboard to authenticated users.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function dashboard()
+    {
+        if(Auth::check())
+        {
+            return view('auth.dashboard');
+        }
+        
+        return redirect()->route('login')
+            ->withErrors([
+            'email' => 'Please login to access the dashboard.',
+        ])->onlyInput('email');
+    } 
+    
+    /**
+     * Log out the user from application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login')
+            ->withSuccess('You have logged out successfully!');;
+    }    
+}
---
+++ database/migrations/2023_11_01_021202_add_picture_to_users.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('photo')->nullable();
+            $table->string('photo_thumbnail')->nullable(); // Thumbnail image
+            $table->string('photo_square')->nullable(); // Square thumbnail image
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('photo','photo_thumbnail','photo_square','photo__square');
+        });
+    }
+};
